bbe315c4dea959e3545d559149a8303f5be2ad99
Ihliana <Ihnatco@protonmail.com>
2023-10-22 20:03:06 +0200

    update script.js
    changing 'active' class when scrolling for different sections

--- js/script.js
+++ js/script.js
@@ -3,27 +3,32 @@
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('nav')
+
+
 window.onscroll = () => {
 
-
-    sections.forEach(sec =>  {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id')
+        let id = sec.getAttribute("id")
 
         if(top >= offset && top < offset + height){
+            //active navbar links 
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             })
         }
+
+
     })
 
 
-
     //sticky header
-    let header = document.querySelector('header')
+    let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 }
